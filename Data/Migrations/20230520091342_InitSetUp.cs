6a965709852faa7448e65788d08d90a60bf1787c
AlexanderNovoselski <87695622+AlexanderNovoselski@users.noreply.github.com>
2023-05-21 16:19:17 +0300

    Add files via upload

---
+++ Data/Migrations/20230520091342_InitSetUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesWebApp.Data.Migrations
+{
+    public partial class InitSetUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WatchedOnDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace MoviesWebApp.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public string Actor { get; set; }
+		public string Genre { get; set; }
+
+		public string ReleaseDate { get; set; }
+		public DateTime WatchedOnDate { get; set; } = DateTime.Now;
+	}
+}
